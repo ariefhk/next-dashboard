18ba03a412892bc95be73d05922a9fbfb2d59a74
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2024-03-03 16:58:34 +0700

    feat: add new

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Customers Page</h1>
+    </div>
+  );
+};
+
+export default Page;
